c1c8eb588ff663536d4858efa350719d487f2596
Albert Haliulov <albert.haliulov@gmail.com>
2019-07-10 11:11:35 +0300

    fixed bug - moved http.get(.. inside app.get(..

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ var os = require('os');
 var http = require('http');
 
 // Configuration
-var backendHost = process.env.BACKEND_HOST || 'devops-demo-backend-internal';
+var backendHost = process.env.BACKEND_HOST || 'localhost';
 var backendPort = process.env.BACKEND_PORT || '9080';
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -19,44 +19,45 @@ app.use("/images", express.static(__dirname + '/static/images'));
 
 var nodePropURL = 'http://' + backendHost + ':' + backendPort + '/demo/api/node/properties';
 var nodeArch, nodePlatform, nodeRelease, nodeHost;
-
-http.get(nodePropURL, function(res){
-    var body = '';
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-    res.on('end', function(){
-        var jsonRes = JSON.parse(body);
-        nodeArch = jsonRes['os.architecture'];
-        nodePlatform = jsonRes['os.name'];
-        nodeRelease = jsonRes['os.version'];
-        nodeHost =  jsonRes['os.hostname'];
-    });
-}).on('error', function(e){
-      console.log("Got an error: ", e);
-});
-
 var podPropURL = 'http://' + backendHost + ':' + backendPort + '/demo/api/pod/properties';
 var podName, podNamespace, podIP, podVersion, podEnv;
-
-http.get(podPropURL, function(res){
-    var body = '';
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-    res.on('end', function(){
-        var jsonRes = JSON.parse(body);
-        podName = jsonRes['pod.name'];
-        podNamespace = jsonRes['pod.namespace'];
-        podIP = jsonRes['pod.ip'];
-        podVersion =  jsonRes['pod.version'];
-        podEnv = jsonRes['pod.environment'];
-    });
-}).on('error', function(e){
-      console.log("Got an error: ", e);
-});
-
+       
 app.get('/demo', function (req, res) {
+
+     http.get(nodePropURL, function(res){
+        var body = '';
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+        res.on('end', function(){
+            var jsonRes = JSON.parse(body);
+            nodeArch = jsonRes['os.architecture'];
+            nodePlatform = jsonRes['os.name'];
+            nodeRelease = jsonRes['os.version'];
+            nodeHost =  jsonRes['os.hostname'];
+        });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+    });
+    
+    
+    http.get(podPropURL, function(res){
+        var body = '';
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+        res.on('end', function(){
+            var jsonRes = JSON.parse(body);
+            podName = jsonRes['pod.name'];
+            podNamespace = jsonRes['pod.namespace'];
+            podIP = jsonRes['pod.ip'];
+            podVersion =  jsonRes['pod.version'];
+            podEnv = jsonRes['pod.environment'];
+        });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+    });
+
     res.render('home', {
       podName: podName || 'unknown',
       podNamespace: podNamespace || 'unknown',
