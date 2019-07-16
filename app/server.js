4e86d2ed3a259fc3b3655960d27a7f401555f21f
Albert Haliulov <albert.haliulov@gmail.com>
2019-07-16 08:18:15 +0300

    made sync http request

--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@ var app = express();
 var os = require('os');
 //var morgan  = require('morgan');
 var http = require('http');
+var request = require('sync-request');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,59 +16,43 @@ app.use("/images", express.static(__dirname + '/static/images'));
 //app.use(express.static('static'));
 //app.use(morgan('combined'));
 
-/*
-var nodePropURL = 'http://' + backendHost + ':' + backendPort + '/demo/api/node/properties';
-var nodeArch, nodePlatform, nodeRelease, nodeHost;
-var podPropURL = 'http://' + backendHost + ':' + backendPort + '/demo/api/pod/properties';
-var podName, podNamespace, podIP, podVersion, podEnv;
-*/
-
 // new vars in release v1.0
 // Backend service URL
 var backendHost = process.env.BACKEND_HOST || 'localhost';
 var backendPort = process.env.BACKEND_PORT || '9080';
 
 var userCheckURL = 'http://' + backendHost + ':' + backendPort + '/demo/api/user/v2/check';
-var user, checkStatus;
+var checkStatus;
+
+// new function in release v1.0
+
+app.post('/check', function (req, res) {
+  var userName=req.body.user;
+  var password=req.body.password;
+  //console.log('User name = ' + userName + ' password is ' + password);
+  var response = request('GET', userCheckURL + '?username=' + userName + '&password=' + password);
+  var jsonRes = JSON.parse(response.getBody('utf8'));
+  checkStatus = jsonRes['status'];
+  /*
+  http.get(userCheckURL + '?username=' + userName + '&password=' + password, function(res){
+      var body = '';
+      res.on('data', function(chunk){
+        body += chunk;
+      });
+      res.on('end', function(){
+        var jsonRes = JSON.parse(body);
+        //user = jsonRes['username'];
+        checkStatus = jsonRes['status'];
+      });
+  }).on('error', function(e){
+      console.log("Got an error: ", e);
+  });
+  */
+    //console.log('Check status: ' + checkStatus);
+    res.redirect('/demo');
+});
        
 app.get('/demo', function (req, res) {
-    
-  /*
-     http.get(nodePropURL, function(res){
-        var body = '';
-        res.on('data', function(chunk){
-            body += chunk;
-        });
-        res.on('end', function(){
-            var jsonRes = JSON.parse(body);
-            nodeArch = jsonRes['os.architecture'];
-            nodePlatform = jsonRes['os.name'];
-            nodeRelease = jsonRes['os.version'];
-            nodeHost =  jsonRes['os.hostname'];
-        });
-    }).on('error', function(e){
-          console.log("Got an error: ", e);
-    });
-    
-    
-    http.get(podPropURL, function(res){
-        var body = '';
-        res.on('data', function(chunk){
-            body += chunk;
-        });
-        res.on('end', function(){
-            var jsonRes = JSON.parse(body);
-            podName = jsonRes['pod.name'];
-            podNamespace = jsonRes['pod.namespace'];
-            podIP = jsonRes['pod.ip'];
-            podVersion =  jsonRes['pod.version'];
-            podEnv = jsonRes['pod.environment'];
-        });
-    }).on('error', function(e){
-          console.log("Got an error: ", e);
-    });
-  */
-    
     res.render('home', {
       podName: process.env.POD_NAME || 'unknown',
       podNamespace: process.env.POD_NAMESPACE || 'unknown',
@@ -78,42 +63,16 @@ app.get('/demo', function (req, res) {
       nodePlatform: os.type(),
       nodeRelease: os.release(),
       nodeHost: os.hostname(),
-      nodeUpTime: secToTime(os.uptime()),
-      user: user || '',
+      //nodeUpTime: secToTime(os.uptime()),
+      //user: user || '',
       checkStatus: checkStatus || 'unknown'
     });
 });
-
-// new function in release v1.0
-
-app.post('/check', function (req, res) {
-  var userName=req.body.user;
-  var password=req.body.password;
-  console.log('User name = ' + userName + ' password is ' + password);
-  
-  http.get(userCheckURL + '?username=' + userName + '&password=' + password, function(res){
-    var body = '';
-    res.on('data', function(chunk){
-        body += chunk;
-    });
-    res.on('end', function(){
-        var jsonRes = JSON.parse(body);
-        user = jsonRes['username'];
-        checkStatus = jsonRes['status'];
-    });
-    }).on('error', function(e){
-      console.log("Got an error: ", e);
-    });
-
-    res.redirect('/demo');
-  });
 
 // Set up listener
 app.listen(8080, function () {
   console.log("Listening on: http://%s:%s", os.hostname(), 8080);
 });
-
-
 
 function secToTime(totalSeconds) {
   hours = Math.floor(totalSeconds / 60 / 60);
